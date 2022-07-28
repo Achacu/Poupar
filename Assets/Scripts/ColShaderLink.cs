1298e592855d9c906e9706bff8a28074e2fd38bb
Achacu <eloicobres@gmail.com>
2022-07-28 22:31:11 +0200

    Now collisions can also show "submeshes". Colliding with a table will also show props on it.

--- Assets/Scripts/ColShaderLink.cs
+++ Assets/Scripts/ColShaderLink.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class ColShaderLink : MonoBehaviour
 {
-    [SerializeField] private MeshRenderer mesh;
+    [SerializeField] private MeshRenderer[] meshes = new MeshRenderer[1];
     // Start is called before the first frame update
-    void Start()
+    void OnValidate()
     {
-        mesh = GetComponent<MeshRenderer>();
+        meshes[0] = GetComponent<MeshRenderer>();
+    }
+    public void Awake()
+    {
+        OnValidate();
     }
 
     private float colExitTime = 0f;
@@ -17,13 +21,19 @@ public class ColShaderLink : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.time - colExitTime < fadeOutTime) mesh.material.SetFloat("_Colliding", 1-(Time.time - colExitTime) / fadeOutTime);
+        if (Time.time - colExitTime < fadeOutTime)
+            for(int i = 0; i < meshes.Length; i++) 
+                meshes[i].material.SetFloat("_Colliding", 1-(Time.time - colExitTime) / fadeOutTime);
     }
     void OnCollisionEnter(Collision collision)
     {
         print("collided with: " +collision.gameObject.name);
-        mesh.material.SetFloat("_Colliding", 1);
-        mesh.material.SetVector("_ColPos", collision.GetContact(0).point);
+        for (int i = 0; i < meshes.Length; i++)
+        {
+            meshes[i].material.SetFloat("_Colliding", 1);
+            meshes[i].material.SetVector("_ColPos", collision.GetContact(0).point);
+        }
+
     }
     void OnCollisionExit(Collision collision)
     {
