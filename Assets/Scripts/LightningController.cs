c1dc4933683ecdd2487470cd0704af732ba418cb
Achacu <eloicobres@gmail.com>
2022-08-03 17:12:48 +0200

    Lightning event added

--- Assets/Scripts/LightningController.cs
+++ Assets/Scripts/LightningController.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class LightningController : MonoBehaviour
 {
+    [SerializeField] private bool playOnAwake = false;
     [SerializeField] private EventSender sender;
+    [SerializeField] private EventSender triggerSender;
     [SerializeField] private Animation anim;
     [SerializeField] private float soundToLightDelay = 0f;
     [SerializeField] private float minCooldown = 2f;
     [SerializeField] private float maxCooldown = 5f;
     // Start is called before the first frame update
+    public void OnEnable()
+    {
+        if (sender) triggerSender.OnActivate += StartLightningLoop;
+    }
+    public void OnDisable()
+    {
+        if (sender) triggerSender.OnActivate -= StartLightningLoop;
+    }
+
     void Start()
     {
-        StartCoroutine(LightningLoop());
+        if(playOnAwake) StartCoroutine(LightningLoop());
     }
+
+    private void StartLightningLoop(EventSender s) => StartCoroutine(LightningLoop());
 
     private IEnumerator LightningLoop()
     {
